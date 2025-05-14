c5f2f7ed07855f91a49121a2c6f68eaf0ff227ee
PaulElcampeon <pauloladele@live.co.uk>
2025-05-14 13:10:24 +0100

    intial

---
+++ src/test/java/com/paulo/taste/TasteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulo.taste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
