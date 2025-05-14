318862cc6c9040417f9fbc3334ca98f11414d7ea
PaulElcampeon <pauloladele@live.co.uk>
2025-05-14 16:43:34 +0100

    updated code

--- src/main/java/com/paulo/taste/HelloController.java
+++ src/main/java/com/paulo/taste/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("/")
     public String hello() {
-        return "Hello, Paul! im taste";
+        return "Hello, Paul! im taste look at me now";
     }
 }
